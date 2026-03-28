44ef1d95ec27fe5cca8b2b22d1aa256c1f87e33e
Jason Zeng <jason.zeng151@gmail.com>
2026-03-27 21:18:27 -0400

    Updated styles for skills to be consistent with rest of app

--- js/apps/skills.js
+++ js/apps/skills.js
@@ -1,3 +1,13 @@
 export function registerSkills(wm) {
-  wm.register('skills', 'Skills - Control Panel', null);
+  wm.register('skills', 'Skills - Control Panel', (body) => {
+    const windowEl = body.closest('.window');
+    windowEl.classList.add('project-window');
+    windowEl.style.height = '340px';
+
+    body.style.flex = '1';
+    body.style.padding = '0';
+    body.style.overflow = 'auto';
+    body.style.minHeight = '0';
+    body.style.background = '#fff';
+  });
 }
